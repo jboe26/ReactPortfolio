6425b244be365fd489791d6a02f13577385befd1
josh boepple <joshrboepple@outlook.com>
2023-10-08 19:36:34 -0400

    upate

---
+++ portfolio-site/src/components/Sidebar/index.js
@@ -0,0 +1,16 @@
+import './index.scss';
+import { Link } from 'react-router-dom';
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+
+
+const Sidebar = () => (
+    <div className='navbar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo" />
+            <img src={LogoSubtitle} alt="logo" />
+        </Link>
+    </div>
+)
+
+export default Sidebar
